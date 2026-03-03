e7ef0bc1a5b7cdb6f4db95552dd777dbf8dd40bb
Otis Johnson <otisj2009@gmail.com>
2026-03-03 12:44:32 +0100

    initial rebranding commit

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(mosquitoR)
+library(vectormodelR)
 
-test_check("mosquitoR")
+test_check("vectormodelR")
