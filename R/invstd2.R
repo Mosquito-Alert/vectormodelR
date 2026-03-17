8948a13440110d2ce728f56b907c51aea5a6a113
Otis Johnson <otisj2009@gmail.com>
2026-03-17 18:39:50 +0100

    removed unusued packaged that were used in MosquitoR but not needed in VectorModelR

--- R/invstd2.R
+++
@@ -1,9 +0,0 @@
-#' Inverse double-standardize a variable
-#'
-#' @param x A numeric vector to be inverse double standardized
-#' @param d The vector used in the original double standardization.
-#' @returns A numeric vector.
-#' @export
-invstd2 = function(x, d) {
-  (x*2*sd(d, na.rm=T))+mean(d,na.rm=T)
-}
--- R/not_covered_by.R
+++
@@ -1,7 +0,0 @@
-#' Simple predicate function for sf to filter all objects that are not covered by another one
-#'
-#' @param x Object to be filtered.
-#' @param y Object used for filtering.
-#' @returns Boolean.
-#' @export
-not_covered_by = function(x, y) !sf::st_covered_by(x, y)
