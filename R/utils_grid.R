307802e0c2777724c9b9dbdfeb61183feeca87dc
Otis Johnson <otisj2009@gmail.com>
2026-03-18 15:22:26 +0100

    tried implementing a single utils function to host multiple small functions

--- R/utils_grid.R
+++ R/utils_grid.R
@@ -1,3 +1,22 @@
+#' Grid utility functions
+#'
+#' Internal/helper functions for snapping coordinates to a regular grid
+#' and creating standard sampling cell IDs.
+#'
+#' @keywords internal
+NULL
+
+#' Round a given number x downward to the nearest n.
+#'
+#' @param x The number to be rounded.
+#' @param n The number to be rounded.
+#' @returns The numeric result of the downward rounding.
+#' @export
+#' @examples
+#' round_down(4.569, 0.05)
+round_down = function(x, n) round( floor( (x*1000)/ (n*1000))*n, decimal_places(n))
+
+
 #' Creates standard sampling cell IDs by masking a set of longitude and latitude values.
 #'
 #' @param lon A vector of longitudes to be masked
