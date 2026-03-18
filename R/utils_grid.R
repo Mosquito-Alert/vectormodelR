bc9cdca7ab6fc52e8bf54ccc1938aeba7d7b0402
Otis Johnson <otisj2009@gmail.com>
2026-03-18 15:59:05 +0100

    updated utils grid with a function for decimal places

--- R/utils_grid.R
+++ R/utils_grid.R
@@ -23,3 +23,18 @@ make_samplingcell_ids = function(lon, lat, mask=0.05){
   masked_lat = round_down(as.numeric(lat), mask)
   return(paste(masked_lon, masked_lat, sep="_"))
 }
+
+#' Return the number of decimal places in a given value
+#'
+#' @param x The value for which the number of decimal places will be returned.
+#' @returns An integer representing the number of decimal places in x.
+#' @noRd
+#' @examples
+#' decimal_places(4.56)
+decimal_places <- function(x) {
+  if ((x %% 1) != 0) {
+    nchar(strsplit(sub('0+$', '', as.character(x)), ".", fixed=TRUE)[[1]][[2]])
+  } else {
+    return(0)
+  }
+}
