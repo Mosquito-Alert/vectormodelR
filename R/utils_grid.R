a78a1a915fe8b232528aea587c14134b2e3d6dbb
Otis Johnson <otisj2009@gmail.com>
2026-03-18 15:30:46 +0100

    namespace changes for util function

--- R/utils_grid.R
+++ R/utils_grid.R
@@ -1,17 +1,9 @@
-#' Grid utility functions
-#'
-#' Internal/helper functions for snapping coordinates to a regular grid
-#' and creating standard sampling cell IDs.
-#'
-#' @keywords internal
-NULL
-
 #' Round a given number x downward to the nearest n.
 #'
 #' @param x The number to be rounded.
 #' @param n The number to be rounded.
 #' @returns The numeric result of the downward rounding.
-#' @export
+#' @noRd
 #' @examples
 #' round_down(4.569, 0.05)
 round_down = function(x, n) round( floor( (x*1000)/ (n*1000))*n, decimal_places(n))
@@ -23,7 +15,7 @@ round_down = function(x, n) round( floor( (x*1000)/ (n*1000))*n, decimal_places(
 #' @param lat A vector of latitudes to be masked
 #' @param mask The masking value.
 #' @returns A character vector of sampling cell IDs.
-#' @export
+#' @noRd
 #' @examples
 #' make_samplingcell_ids(lon=c(2.1686, 2.1032), lat=c(41.3874, 41.2098), 0.05)
 make_samplingcell_ids = function(lon, lat, mask=0.05){
